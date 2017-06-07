52fb0f407c6f3c2b1142c1d55ee43b11be082aa1
Farrukh Askari <farrukh.askari01@gmail.com>
2017-06-07 13:15:51 +0500

    Initial Commit

---
+++ ABC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ABC
+//
+//  Created by Syed Askari on 07/06/2017.
+//  Copyright © 2017 Askari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
